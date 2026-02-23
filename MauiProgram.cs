20a73a764adeb79c18858d36df8e7e8f8c9b1c40
Salty Duck <142966149+Megaloduck@users.noreply.github.com>
2026-02-23 16:33:51 +0700

    commit before publish

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -24,13 +24,7 @@ namespace Voxelize
     					handler.PlatformView.SingleSelectionFollowsFocus = false;
     				});
 
-    				Microsoft.Maui.Handlers.ContentViewHandler.Mapper.AppendToMapping(nameof(Pages.Controls.CategoryChart), (handler, view) =>
-    				{
-    					if (view is Pages.Controls.CategoryChart && handler.PlatformView is Microsoft.Maui.Platform.ContentPanel contentPanel)
-    					{
-    						contentPanel.IsTabStop = true;
-    					}
-    				});
+    				
 #endif
                 })
                 .ConfigureFonts(fonts =>
